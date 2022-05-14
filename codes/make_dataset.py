631a46f5bfd1a89dfc6c8edfae3cabc343ef5afd
yousef <mr.ysaj@gmail.com>
2022-05-14 09:08:53 +0430

    [UPDATE] the program updated for make a new branch for finding solution for def find label

--- codes/make_dataset.py
+++ codes/make_dataset.py
@@ -1,14 +1,13 @@
-import mistune
 import pandas as pd
 import logging
 import re
+from nltk import tokenize
+from nltk.tokenize import word_tokenize
+
 logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
-from nltk import pos_tag, RegexpParser, tokenize
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from sklearn.preprocessing import LabelEncoder
+
 
 def split_by_sentences(text):
     """use NLTK Tokenize for split text file with sentences"""
@@ -18,9 +17,32 @@ def split_by_sentences(text):
 
 def find_labels(text, skills):
     """find labels of each word"""
-    #TODO-01 write a code for finded skill- and o type
+    text_token = word_tokenize(text)
+    # TODO-01 write a code for found "skill" and "O" type
+    data_frame = pd.DataFrame({"sentences_id": " ",
+                               "token": text_token,
+                               "labels": " ",
+                               })
+    labels_list = []
+    final_label_list = []
+    token_index = [(index, tok) for index, tok in enumerate(list(data_frame["token"]))]
     for skill in skills:
-        pass
+        skill_token = skill.split(" ")
+        if len(skill_token) > 1:
+            for num in range(len(text_token)):
+                check_token = text_token[num: len(skill_token) + num]
+                check_str = " ".join(check_token)
+                if skill == check_str:
+                    first = skill_token[0]
+                    labels = list(map(lambda x: [x, "B-SKILL"] if x == first else [x, "I-SKILL"], skill_token))
+                    labels_list.append(labels)
+
+        else:
+            labels_list.append([skill, "B-SKILL"])
+    for label in labels_list:
+        if not label in final_label_list:
+            final_label_list.append(label)
+
 
 def split_by_token(sentences):
     return word_tokenize(sentences)
@@ -35,7 +57,6 @@ def split_sentences_token(sentences_list, labels):
 
         }, columns=["sentences_id", "words", "labels"])
         for index, sentence in enumerate(sentences_list):
-
             token = split_by_token(sentence)
             data_frame = pd.DataFrame({
                 f"sentences_id": f"{index}",
@@ -47,6 +68,8 @@ def split_sentences_token(sentences_list, labels):
         raise Exception("You must give the function a list of sentences")
 
     return main_dataframe
+
+
 def get_similar_word(sentence, skills):
     """use diff-lib to get most similar word to skill"""
     if isinstance(sentence, list):
@@ -88,7 +111,6 @@ def extract_token(sentences_list, labels):
 
 
 with open('dataset/linkdin-skills/linkedin_skills.txt', "r", encoding="utf-8") as linkdin_skills:
-
     skills = linkdin_skills.read()
     skills_list = skills.split("\n")
     skills_list = list(map(lambda x: x.lower(), skills_list))
@@ -98,4 +120,3 @@ with open("dataset/About/zhiyunren.txt", "r", encoding="utf-8") as text:
     founded_skill = get_similar_word(sentence=sentences_list, skills=skills_list)
     labels = find_labels(text, founded_skill)
     extract_token(sentences_list, founded_skill)
-
